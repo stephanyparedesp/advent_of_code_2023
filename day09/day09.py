8102312793e761adf09bba663e6f917a8c0ec03b
Stephany Paredes <stephany.paredes@hafslundeco.no>
2023-12-09 15:28:10 +0100

    small rewrite day09

--- day09/day09.py
+++ day09/day09.py
@@ -2,37 +2,35 @@ import numpy as np
 
 fn = "data.txt"
 lines = open(fn).read().split("\n")
-# Part 1
-adds = 0
-for line in lines:
-    oasis = [int(a) for a in line.split()]
+
+
+def get_diffs(oasis, reversed=False):
     diff_series = [oasis]
     diff = np.diff(oasis)
     while sum(diff) != 0:
         diff_series.append(diff)
         diff = np.diff(diff)
-    # Now we have a list of differences
+    if reversed:
+        diff_series.reverse()
+    return diff_series
+
+
+# Part 1
+adds = 0
+for line in lines:
+    oasis = [int(a) for a in line.split()]
     sum_e = 0
-    for e in diff_series:
+    for e in get_diffs(oasis):
         sum_e += e[-1]
     adds += sum_e
-
 print(adds)
 # Part 2
 adds = 0
 for line in lines:
     oasis = [int(a) for a in line.split()]
-    diff_series = [oasis]
-    diff = np.diff(oasis)
-    while sum(diff) != 0:
-        diff_series.append(diff)
-        diff = np.diff(diff)
-    # Now we have a list of differences
     vals = [0]
-    diff_series.reverse()
-    for e in diff_series:
+    for e in get_diffs(oasis, reversed=True):
         val = e[0] - vals[-1]
         vals.append(val)
     adds += vals[-1]
-
 print(adds)
